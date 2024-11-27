9d4e7c431848061e4b267dbd74669f80f8af4893
Ganeshan Venu <95889744+gvenu06@users.noreply.github.com>
2024-11-26 22:59:59 -0500

    UpdatedFunctionalInterface
    We realized we made a mistake with our original commit and this is the updated functional interface

--- addNumbers.java
+++ addNumbers.java
@@ -1,16 +1,6 @@
 @FunctionalInterface
 public interface addNumbers {
     int addNumbersMethod(int start, int end);
-
-    default int addNumbersMethodFarees(int start, int end)
-    {
-        int sum=0;
-        for(int x=start;x<=end;x++)
-        {
-            sum+=x;
-        }
-        return sum;
-    }
 }
 
 
