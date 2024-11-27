7de663aee4bccc76cad5aa877cca9ca108149eb4
Ganeshan Venu <95889744+gvenu06@users.noreply.github.com>
2024-11-26 23:26:28 -0500

    LambdaExpression
    Created an instance of interface and used lambda expression to assign it a value

--- PartnerMain.java
+++ PartnerMain.java
@@ -1,11 +1,19 @@
 public class PartnerMain
 {
-	
 	public static void main(String[]args)
 	{
+		addNumbers farees = (start, end) -> {
+    
+        int sum=0;
+        for(int x=start;x<=end;x++)
+        {
+            sum+=x;
+        }
+        return sum;
+    
+		};
 
+		System.out.println(farees.addNumbersMethod(1, 3));
 	}
-
-
 }
 
